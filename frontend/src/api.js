596ae0edae2e63c1d77773ac294701cf8fcf1da6
Ella Victor <Vickytorella@gmail.com>
2025-08-03 00:37:05 +0100

    changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/djangoreact/backend/v1";
+const apiUrl = "https://9738dfd3-5cba-44d7-b45b-cc4a1bcd846c-dev.e1-us-east-azure.choreoapis.dev/djangoreact/backend/v1.0";
+
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
